ce16eecef749f6e85ceb30bad05ba99744ac75fe
arycodes0 <alvarezaryam@gmail.com>
2024-09-13 15:54:15 -0400

    chore: Add routes

--- run.py
+++ run.py
@@ -1,1 +1,22 @@
-#Entry point for running the application
+# Entry point for running the application.
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# Route for the index page.
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# Route for the login page.
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+# Route for the signup page.
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
