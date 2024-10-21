5c9d0a98e9f7843c75fc5901f8c7e8c41d2fdc22
kriverao98 <riveraocasiokoral29@gmail.com>
2024-10-21 19:03:24 -0400

    Refactor CSS and JavaScript files for improved styling and functionality

--- static/js/app.js
+++ static/js/app.js
@@ -85,6 +85,37 @@ async function handleLogin(email, password) {
   }
 }
 
+//Home Page
+
+// Google maps api, copied directly from google
+// Initialize and add the map
+let map;
+
+async function initMap() {
+  // The location of Uluru
+  const position = { lat: -25.344, lng: 131.031 };
+  // Request needed libraries.
+  //@ts-ignore
+  const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+
+  // The map, centered at Uluru
+  map = new Map(document.getElementById("map"), {
+    zoom: 4,
+    center: position,
+    mapId: "DEMO_MAP_ID",
+  });
+
+  // The marker, positioned at Uluru
+  const marker = new AdvancedMarkerElement({
+    map: map,
+    position: position,
+    title: "Uluru",
+  });
+}
+
+initMap();
+
 //DOMContentLoaded Event Listener
 document.addEventListener('DOMContentLoaded', async () => {
   console.log('DOM fully loaded and parsed.');
